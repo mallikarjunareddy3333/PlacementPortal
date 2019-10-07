ffa929d4e8bd7ac3604469ffe05aff16347e6a94
mallikarjuna_reddy_giddam <mallikarjunareddy3333@gmail.com>
2019-10-08 01:21:50 +0530

    - implemented the session management

--- ecms_ui/src/app/_guard/auth.guard.ts
+++ ecms_ui/src/app/_guard/auth.guard.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthenticationService } from '../_services/authentication.service';
+import { Role } from '../_models/Role';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
     constructor(
-        private router: Router
+        private router: Router,
+        private authenticationService: AuthenticationService
     ){}
     
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
-        let token = window.localStorage.getItem('token');
-        if(!token) {
-            this.router.navigate(['/']);
-            return false;
+        const currentUser = this.authenticationService.currentUserValue;
+        if (currentUser) {
+            if(route.data.roles.some((val) => currentUser.roles.indexOf(val) !== -1)  ){
+                return true;
+            } else {
+                // not logged in so redirect to login page with the return url
+                this.router.navigate(['/'], { queryParams: { returnUrl: state.url }});
+                return false;
+            }
         } else {
-            return true;
+            // not logged in so redirect to login page with the return url
+            this.router.navigate(['/'], { queryParams: { returnUrl: state.url }});
+            return false;
         }
     }
 
